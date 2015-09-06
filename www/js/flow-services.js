5588d969e1b5594ae3d5809247af2e9abf56fbbb
DuongTDN <DuongTDN@gmail.com>
2015-09-06 20:14:08 +0700

    logic for navigating accross scene, topic and lesson

--- www/js/flow-services.js
+++ www/js/flow-services.js
@@ -1,7 +1,7 @@
 
 var flowModule = angular.module('flowModule',[]);
 
-flowModule.factory('Flow', ['$routeParams', '$http', 'Video', function ($routeParams, $http, Video) {
+flowModule.factory('Flow', ['$routeParams', '$http', '$location', 'Video', function ($routeParams, $http, $location, Video) {
 	
 	var SERVER = {
 		COURSE : "php/CourseInfo.php",
@@ -94,10 +94,26 @@ flowModule.factory('Flow', ['$routeParams', '$http', 'Video', function ($routePa
 			loadScene(tid, sid);
 		},
 		
-		next : function() {
-			tid = 0;
-			sid ++;
-			loadScene(tid, sid);
+		next : function() {			
+			if (tid !== 'undefined') {
+				sid ++;
+				if (sid == lesson.topic[tid].scene.length) { // last scene					
+					sid = 0;
+					tid++;
+					if (tid == lesson.topic.length) { // last topic
+						tid = 0;
+						var currPath = $location.path(),						
+							index = currPath.lastIndexOf('/'),
+							rootPath = currPath.substr(0,index);
+							nextLesson = parseInt(currPath.substr(index+1),10) + 1;
+						// need proper check for that this is the last lesson
+						var nextPath = rootPath + '/' + nextLesson;
+						$location.path(nextPath);
+					}
+				}
+				loadScene(tid, sid);
+			}
+			console.log (tid + ' ' + sid);
 		},
 		
 		back : function() {
