65f4e668a0f5d4a42775f3d1db64a8d00e3c38cd
DuongTDN <DuongTDN@gmail.com>
2015-09-09 00:27:12 +0700

    added layout for class lesson progress bar

--- www/js/lesson-controller.js
+++ www/js/lesson-controller.js
@@ -5,14 +5,40 @@ function LessonCtrl($scope, Flow, Video){
 	
 	$scope.lid = Flow.lid();
 
+	// navigation logic
 	$scope.next = Flow.next;
 	$scope.back = Flow.back;
 	
+	var screenWidth = 1170,	// need to detect screen width
+		spacing = 3;
+	
+	
+	
+	// async request for lesson data
 	Flow.getLesson(function(data){
-		$scope.lesson = data;	
+		$scope.lesson = data;
+		$scope.topics = $scope.lesson.topic;
+		var standardBarWidth = (screenWidth + spacing)/$scope.topics.length - spacing,
+			lastBarWidth = screenWidth - (standardBarWidth + spacing) * ($scope.topics.length - 1);
+		// prepare bar
+		$scope.bars = [];
+		for (var i=0, len=$scope.topics.length-1; i < len; i++) {			
+			$scope.bars.push({
+				width : standardBarWidth + 'px',
+				spacing : spacing + 'px',
+				complete : '100%'
+			});
+		}
+		// for the last
+		$scope.bars.push({
+			width : lastBarWidth + 'px',
+			spacing : '0px',
+			complete : '60%'
+		});
 		//console.log (data);
 	})
 
+	// special treatment for youtube async api load
 	$scope.youtubeReady = function() {
 		Video.setOnFinish ('LessonCtrl', Flow.next);
 		Video.setYoutubeApiReady(true);
@@ -21,6 +47,9 @@ function LessonCtrl($scope, Flow, Video){
 			Flow.loadPlayboard();
 		}
 	}
+	
+	// progress bar
+	
 	
 }
 LessonCtrl.$inject = ['$scope', 'Flow', 'Video'];
